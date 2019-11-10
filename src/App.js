48a9bb3ec173950380a76578054809e442d48cb1
Joshua Tennies <jdtennies10@gmail.com>
2019-11-10 13:39:11 -0500

    Complete navbar except one style issue

--- src/App.js
+++ src/App.js
@@ -5,23 +5,28 @@ import NavBar from './components/NavBar.js';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path='/blog'>
-          <h1>Blog placeholder</h1>
-        </Route>
-        <Route path='/projects'>
-          <h1>Projects placeholder</h1>
-        </Route>
-        <Route path='/about'>
-          <h1>About me placeholder</h1>
-        </Route>
-        <Route path='/'>
-          <NavBar/>
-        </Route>
-        <Redirect from='*' to='/'/>
-      </Switch>
-    </Router>
+
+      <Router>
+        <Switch>
+          <Route path='/blog'>
+            <NavBar/>
+            <h1>Blog placeholder</h1>
+          </Route>
+          <Route path='/projects'>
+            <NavBar/>
+            <h1>Projects placeholder</h1>
+          </Route>
+          <Route path='/about'>
+            <NavBar/>
+            <h1>About me placeholder</h1>
+          </Route>
+          <Route path='/'>
+           <NavBar/>
+            <p>Content here</p>
+          </Route>
+          <Redirect from='*' to='/'/>
+        </Switch>
+      </Router>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import tennisBall from '../assets/tennisball.PNG';
+import '../assets/styling.css';
 
 class NavBar extends React.PureComponent {
     rerouter = (path) => {
@@ -9,11 +10,11 @@ class NavBar extends React.PureComponent {
 
     render() {
         return (
-            <div>
-                <Link to='/'><img src={tennisBall}/></Link>
-                <Link to='/projects'>Projects</Link>
-                <Link to='/blog'>Blog</Link>
-                <Link to='/about'>About</Link>
+            <div className='NavBar'>
+                <div><Link className='Link' to='/'><img src={tennisBall}/></Link></div>
+                <Link className='Link' to='/projects'>Projects</Link>
+                <Link className='Link' to='/blog'>Blog</Link>
+                <Link className='Link' to='/about'>About</Link>
             </div>
         );
     };
