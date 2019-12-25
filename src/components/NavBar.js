9a42f894d969ed2d6449993984cefdd6a47d56ee
Joshua Tennies <jdtennies10@gmail.com>
2019-12-24 21:03:52 -0500

    Adjusted icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import black_menu from '../assets/black_menu.png';
-import white_menu from '../assets/white_menu.png';
-import table_tennis from '../assets/table-tennis.png';
+import menu_black from '../assets/menu-black.png';
+import menu_white from '../assets/menu-white.png';
+import table_tennis_black from '../assets/table-tennis-black.png';
+import table_tennis_white from '../assets/table-tennis-white.png';
 import BlogList from './BlogList.js';
 import '../assets/styling.css';
 
@@ -12,7 +13,8 @@ class NavBar extends React.PureComponent {
         this.state = {
             menuDisplay: 'none',
             navBarClass: 'nav-bar',
-            menuImage: black_menu,
+            menuImage: menu_black,
+            mobileIcon: table_tennis_black,
             isMobile: this.setIsMobile()
         }
     }
@@ -27,13 +29,15 @@ class NavBar extends React.PureComponent {
                 this.setState({
                     menuDisplay: 'none',
                     navBarClass: 'mobile-nav-bar',
-                    menuImage: black_menu
+                    menuImage: menu_black,
+                    mobileIcon: table_tennis_black
                 })
             } else {
                 this.setState({
                     menuDisplay: 'block',
                     navBarClass: 'mobile-nav-bar-expanded',
-                    menuImage: white_menu
+                    menuImage: menu_white,
+                    mobileIcon: table_tennis_white
                 }) 
             }
         } else {
@@ -41,13 +45,13 @@ class NavBar extends React.PureComponent {
                 this.setState({
                     menuDisplay: 'none',
                     navBarClass: 'nav-bar',
-                    menuImage: black_menu
+                    menuImage: menu_black
                 })
             } else if(this.state.menuDisplay == 'none') {
                 this.setState({
                     menuDisplay: 'block',
                     navBarClass: 'nav-bar-expanded',
-                    menuImage: white_menu
+                    menuImage: menu_white
                 })
             }
         }
@@ -78,7 +82,7 @@ class NavBar extends React.PureComponent {
             <div className={this.state.navBarClass}>
                 <div className='nav-bar-simple'>
                     <button className='menu-button' onClick={() => {this.toggleMenu()}}><img src={this.state.menuImage}/></button>
-                    <h3>{(this.state.isMobile) ? <img src={table_tennis}/> : 'Table10is'}</h3>
+                    <a href='/'><h3>{(this.state.isMobile) ? <img src={this.state.mobileIcon}/> : 'Table10is'}</h3></a>
                 </div>
                 <div className='menu-options' style={{display:this.state.menuDisplay}}>
                     <Link className='link' to='/'><h2>Home</h2></Link>
