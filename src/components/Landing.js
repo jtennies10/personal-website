f79be1632994ff9dc277e10481ae7f4e612f751f
Joshua Tennies <jdtennies10@gmail.com>
2020-01-08 22:32:02 -0500

    Formatting changes and typo fixes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -18,7 +18,7 @@ class Landing extends React.PureComponent {
                         While I love coding, my real passion lies in helping people and trying to be a better reflection of Jesus everyday. Beyond that, interests include adventuring, trying new things, movie-going, and fitness. Oh, and of course video games.
                         <br/><br/>This site is in its infancy and will be pretty bare for awhile as I 
                             progressively add to it. Slowly I will turn this place into a portfolio of my personal work, 
-                            as well a place I can leave my thoughts for the world to see (or at least the three of four people that end up checking this place out). 
+                            as well a place I can leave my thoughts for the world to see (or at least the three or four people that end up checking this place out). 
                             If you’d like to reach out to me, you can reach me at jdtennies10@gmail.com.</p>
                     </div>
                 </div>
